d1276dce1c6eeb32bd0e89973e32631fc5da2f5c
Lynn-Ngx <C15426672@mydit.ie>
2017-03-04 16:09:01 +0000

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,11 +1,11 @@
 import processing.core.PVector;
 
-// Class for the Car
+//Class for the Car
 public class Car extends GameObject {
     public void draw() {
         Game.getInstance().image(Resources.getCar(), position.x, position.y);
 
-        // Controls for the movement of the car
+        //Controls for the movement of the car
         if (Game.getInstance().keyPressed) {
             if (Game.getInstance().key == 'a') {
                 position.x -= 5;
@@ -28,7 +28,8 @@ public class Car extends GameObject {
     public void start() {
         position = new PVector(Game.getInstance().width / 2 - (Resources.getCar().width / 2), Game.getInstance().height - Resources.getCar().height);
     }
-
+    
+    //Resets game when die
     public void die() {
         Game.getInstance().reset();
     }
